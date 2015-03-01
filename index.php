e53798d4664f29c659f2f32407bb3ae6330c1fbf
Thomas <thomastoodo@msn.com>
2015-03-01 16:43:55 +0200

    no message

--- index.php
+++ index.php
@@ -4,43 +4,47 @@
         <title>E-hääletus</title>
         <meta charset="utf-8">
 
-        <!--[if lt IE 9]>
-        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js">
-        </script>
-        <![endif]-->
+        
         <link rel="stylesheet" href="styles.css">
     </head>
 
     <body>
 
         <header>
+        	<h1> E-Valimised</h1>
             <nav>
-                <ul>
+                
+                <ul>	
                     <li>
-                        <h1>E-hääletusafasfasfasds</h1>
-                    </li>
-
-                    <li>
-                        <a href="html5_semantic_elements.asp">HTML5 Semantic</a>
+                        <a href="Kandidaadid.php">Kandidaadid</a>
                     </li>
                     <li>
-                        <a href="html5_geolocation.asp">HTML5 Geolocation</a>
+                        <a href="Tulemused.php">Tulemused</a>
                     </li>
                     <li>
-                        <a href="html5_canvas.asp">HTML5 Graphics</a>
+                        <a href="Statistika.php">Statistika</a>
                     </li>
+                    <li>
+                        <a href="Minuandmed.php">Minu andmed</a>
+                    </li>
+                    <li>
+                        <a href="login.php">Logi sisse</a>
+                    </li>
+                    
+                    
+                   
                 </ul>
             </nav>
         </header>
-
+		<hr>
         <section>
 
-            <h1>Tere tulemast e-hääletuse veebilehele1</h1>
+            <h2>Tere tulemast e-hääletuse leheküljele!</h2>
 
             <article>
-                <h2>Lalala</h2>
+                
                 <p>
-                    <?php echo "asd";  ?>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum, lacus vitae sollicitudin varius, tellus neque pharetra diam, posuere pharetra sapien sapien non tellus. Fusce nec laoreet neque, non ullamcorper mi. Fusce ac sem nulla. Sed quis ante at felis elementum ornare faucibus sed mauris. Mauris sodales tempus nisl, quis convallis massa aliquam vitae. Cras finibus erat feugiat, consequat lacus eu, posuere ligula. Suspendisse lectus libero, consectetur vel tellus ac, tristique egestas ex.
                 </p>
             </article>
 
@@ -48,7 +52,8 @@
 
         <footer>
             <p>
-                &copy; 2014 W3Schools. All rights reserved.
+            <hr>
+                &copy; erdnah/evoting
             </p>
         </footer>
 
