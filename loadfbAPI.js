40e512d9902757a85da17aab4f3a1902a4b1bf39
Handre Elias <handreelias@gmail.com>
2015-03-22 20:58:53 +0200

    andmed

---
+++ loadfbAPI.js
@@ -0,0 +1,24 @@
+window.fbAsyncInit = function() {
+	FB.init({
+		appId : '869773549753764',
+		cookie : true, // enable cookies to allow the server to access
+		// the session
+		xfbml : true, // parse social plugins on this page
+		version : 'v2.2' // use version 2.2
+	});
+
+	FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+	});
+
+};
+
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id))
+		return;
+	js = d.createElement(s);
+	js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
