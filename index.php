c6f6c0e26af454497272c51d6ec63abd7e2093ff
Handre Elias <handreelias@gmail.com>
2015-03-24 11:20:23 +0200

    Animation

--- index.php
+++ index.php
@@ -29,14 +29,24 @@
                 Logi sisse
             </li>
         </ul>
-        <ul id="tab">
-            <li class="active">
+        <div class="tabs">
+            <ul class="tab-links">
+                <li class="active"><a href="#tab1">Avaleht</a></li>
+                <li><a href="#tab2">Kandidaadid</a></li>
+                <li><a href="#tab3">Tulemused</a></li>
+                <li><a href="#tab4">Minu andmed</a></li>
+                <li><a href="#tab5">Logi sisse</a></li>
+            </ul>
+        </div>
+        <div class="tab-content">
+            <div id="tab1" class="tab active">
                 <h2 id="teretulemast">Tere tulemast e-hääletuse lehele!</h2>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum, lacus vitae sollicitudin varius, tellus neque pharetra diam, posuere pharetra sapien sapien non tellus. Fusce nec laoreet neque, non ullamcorper mi. Fusce ac sem nulla. Sed quis ante at felis elementum ornare faucibus sed mauris. Mauris sodales tempus nisl, quis convallis massa aliquam vitae. Cras finibus erat feugiat, consequat lacus eu, posuere ligula. Suspendisse lectus libero, consectetur vel tellus ac, tristique egestas ex.
                 </p>
-            </li>
-            <li>
+            </div>
+ 
+            <div id="tab2" class="tab">
                 <h2>Kandidaatide tabel</h2>
                 <div class="datagrid">
                 <table>
@@ -59,8 +69,9 @@
                     </tbody>
                 </table>
                 <div>
-            </li>
-            <li>
+            </div>
+ 
+            <div id="tab3" class="tab">
                 <h2>Tulemused</h2>
                 Tulemused kandidaatide järgi:
                 <div class="datagrid">
@@ -105,18 +116,18 @@
                     </tbody>
                 </table>
                 </div>
-            </li>
-            <li>
+            </div>
+ 
+            <div id="tab4" class="tab">
                 <h2>Minu andmed</h2>
                 <div id="status"></div>
 
                 <div id="mant"></div>
-
-            </li>
-            <li>
+            </div>
+            <div id="tab5" class="tab">
                 <h2 id="logi2">Sisselogimine</h2>
                 <div class="fb-login-button" data-max-rows="1" data-size="xlarge" onlogin="checkLoginState();" data-show-faces="false" data-auto-logout-link="true"></div>
-            </li>
-        </ul>
+            </div>
+        </div>
     </body>
 </html>
