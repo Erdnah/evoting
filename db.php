1e07ac55e40c2abf4d947a073e592dcc96495fc9
Thomas <thomastoodo@msn.com>
2015-04-05 16:06:52 +0300

    eqwe

--- db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$connection = mysql_connect('tcp:vzwses3zoj.database.windows.net','evoting@vzwses3zoj','Salasona123') or die(mysql_error());
-$database = mysql_select_db('evoting') or die(mysql_error());
-?>
