e68c0937615bfcc4600a26a6471c4b5dbe13c9c3
Handre Elias <handreelias@gmail.com>
2015-04-17 11:07:35 +0300

    Animation

--- js/tabs.js
+++ js/tabs.js
@@ -3,10 +3,15 @@ $(document).ready(function(){
         if (!$(this).hasClass("active")) {
             var tabNum = $(this).index();
             var nthChild = tabNum+1;
+            
             $("ul#tabs li.active").removeClass("active");
             $(this).addClass("active");
-            $("ul#tab li.active").removeClass("active");
-            $("ul#tab li:nth-child("+nthChild+")").addClass("active");
+            $("ul#tab li.active").fadeOut(100, function() {
+            	$("ul#tab li.active").removeClass("active");
+                $("ul#tab li:nth-child("+nthChild+")").fadeIn(100);
+                $("ul#tab li:nth-child("+nthChild+")").addClass("active");
+			});
+            
         }
     });
 });
