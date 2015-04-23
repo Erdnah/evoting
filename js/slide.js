26ec70eeb8b27e7e73f6aeb893620d67300b6237
Handre Elias <handreelias@gmail.com>
2015-04-23 23:31:12 +0300

    Hmm

--- js/slide.js
+++ js/slide.js
@@ -1,16 +1,15 @@
 $(".clickable").click(function() {
 	var id = this.children[0].textContent;
-	var nimi = this.children[1].textContent;
-	if (document.getElementById('hidden') != null) {
-		document.getElementById('hidden').id = '';
-	}
-	document.getElementsByClassName('votebtn')[0].addEventListener("click", function() {
+	var name = this.children[1].textContent;
+	document.getElementById('votebtn').addEventListener("click", function() {
 	    postVote(id);
 	}, false);
-	//console.log(this.children[0].textContent);	
+	//console.log(this.children[0].textContent);
+	
 	$("#info").slideToggle(100, function() {
 		document.getElementById('infotekst').innerHTML = 'Siia peaks tulema ' + 
-		nimi + ' info varsti.\n';
+		name + ' info varsti.\n';
+		document.getElementById('votebtn').className = '';
 		$("#info").slideToggle(100, function() {
 		    // Animation complete.
 		  });
