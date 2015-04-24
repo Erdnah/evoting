030694ec5068b2654bac2dfb047ff6c65aeadc0c
Handre Elias <handreelias@gmail.com>
2015-04-24 13:11:10 +0300

    wtf

--- getkandidaat.php
+++ getkandidaat.php
@@ -8,5 +8,6 @@ $kandidaat = getKandidaat($id, 'id');
 $kandidaat = $kandidaat[0];
 echo "<img src=\"//graph.facebook.com/$kandidaat[1]/picture??width=200&height=200\">
 <br>
+Id: $kandidaat[1] <br>
 Info: $kandidaat[4]";
 
