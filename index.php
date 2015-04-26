c6f135f6cf44e5980d145c7de23106f94d48ae91
Thomas <thomastoodo@msn.com>
2015-04-26 20:49:37 +0300

    x

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html lang="en" xmlns:fb="http://www.facebook.com/2008/fbml">
-<html>
+<!DOCTYPE html>
+<html lang="en" xmlns:fb="http://www.facebook.com/2008/fbml">
     <head>
         <meta charset="UTF-8">
         <title>E-hääletus</title>
