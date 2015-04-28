5a28b3049d3ae6ec3735687b51baac87d4c2686d
Handre Elias <handreelias@gmail.com>
2015-04-28 11:44:23 +0300

    x

--- js/slide.js
+++ js/slide.js
@@ -1,16 +1,16 @@
 $("#kandidaadidTabel").on("click", ".clickable", function() {
 	var id = this.children[0].textContent;
 	var name = this.children[1].textContent;
-	document.getElementById('votebtn').addEventListener("click", function() {
-	    postVote(id);
-	}, false);
-	//console.log(this.children[0].textContent);
+	console.log('clicked ' + id);
 	var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         	$("#info").slideToggle(100, function() {
         		document.getElementById("infotekst").innerHTML = xmlhttp.responseText;
         		document.getElementById('votebtn').className = '';
+        		document.getElementById('votebtn').addEventListener("click", function() {
+        		    postVote(id);
+        		}, false);
         		$("#info").slideToggle(100, function() {
         		    // Animation complete.
         		  });
@@ -19,7 +19,7 @@ $("#kandidaadidTabel").on("click", ".clickable", function() {
 			
         }
     }
-    xmlhttp.open("POST", "getkandidaat.php?id=" + id, true);
+    xmlhttp.open("GET", "getkandidaat.php?id=" + id, true);
     xmlhttp.send();
 	
 });
