5957263f10ecfceac647c64d0ef2de3cc7de03b4
Handre Elias <handreelias@gmail.com>
2015-05-12 12:03:42 +0300

    Regular polling

--- js/longpolling.js
+++ js/longpolling.js
@@ -1,30 +1,26 @@
 function messages_longpolling(rows) {
 	var t;
-	
-	if (typeof lastId == 'undefined') {
-		lastId = 0;
-	}
 
 	jQuery.ajax({
 		url : 'longpoll.php',
 		type : 'GET',
-		data : 'startrows=' + rows,
 		dataType : 'json',
 		success : function(payload) {
 			clearInterval(t);
 			t = setTimeout(function() {
 				messages_longpolling(payload.num_rows);
-			}, 1000);
-			if (payload.status == 'results' || payload.status == 'no-results') {				
-				
-				if (payload.status == 'results') {
+			}, 2000);
+
+			if (payload.status == 'results') {
+				if (payload.num_rows != rows) {
 					console.log('Häälte arv muutunud');
 					updateTulemused();
 				}
-			}		
+				
+			}
 		},
 		error : function(jqXHR, textStatus, errorThrown) {
-			console.log(jqXHR.status + " "+ textStatus  + " "+errorThrown);
+			console.log(jqXHR.status + " " + textStatus + " " + errorThrown);
 			clearInterval(t);
 			t = setTimeout(function() {
 				messages_longpolling(0);
--- longpoll.php
+++ longpoll.php
@@ -1,24 +1,9 @@
 <?php
 require_once ('taskmodel.php');
 
-$time_wasted = 0;
 $new_messages_check = getVotesAfter();
-$num_rowsbefore = $_GET['startrows'];
 $num_rows = count($new_messages_check);
-while (true) {
-    if ($time_wasted >= 60) {
-        die(json_encode(array('status' => 'no-results', 'num_rows' => $num_rows, 'before' => $num_rowsbefore)));
-        exit ;
-    }
-    sleep(2);
-    $new_messages_check = getVotesAfter();
-    $num_rows = count($new_messages_check); 
-    if ($num_rows != $num_rowsbefore) {
-        die(json_encode(array('status' => 'results', 'num_rows' => $num_rows)));
-        exit ;
-    }    
-    $time_wasted += 1;
-}
+die(json_encode(array('status' => 'results', 'num_rows' => $num_rows)));
+exit ;
 ?>
- 
 
